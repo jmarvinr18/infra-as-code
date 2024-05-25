121a27a360e3b75a465f24b70b50b15df3976f0c
Marvin <josemarvin.ra@reliasourcing.com>
2024-05-25 08:27:15 +0800

    added ansible and terraform

---
+++ terraform/modules/aws/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ami" {
+  default = ""
+}
+
+variable "security_groups" {
+  default = []
+}
---
+++ terraform/projects/dos2win/aws/staging/ec2/main.tf
@@ -0,0 +1,14 @@
+module "sg_module" {
+  source = "../../../../../modules/aws/sg"
+}
+
+module "ec2_module" {
+  source = "../../../../../modules/aws/ec2"
+  
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-07bf1d1b4c1225623"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [module.sg_module.sg_id]
+}
