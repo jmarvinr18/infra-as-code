fc46064f6a401eb4d9ea1f16cd48a42c5e8faa76
Marvin <josemarvin.ra@reliasourcing.com>
2024-08-05 18:42:20 +0800

    Revert "added tf module for creating iam user"
    This reverts commit 9da2b27618104e76bba18b3d773e1dadb6b8dfd8.

--- terraform/projects/torrospin/production/iam/main.tf
+++
@@ -1,5 +0,0 @@
-module "iam_user" {
-  source         = "../../../../modules/aws/iam/user"
-  iam_user_name  = var.iam_user_name
-  policy_details = var.policy_details
-}
--- terraform/projects/torrospin/production/iam/output.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = module.iam_user.id
-}
-
-output "secret" {
-  value = module.iam_user.secret
-}
