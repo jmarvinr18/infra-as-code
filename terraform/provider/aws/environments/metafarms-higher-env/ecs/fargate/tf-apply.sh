1c132b24ef3b9f6c9489495538646baa2d4ed50e
jmarvinr18 <marvin@xctuality.com>
2025-05-24 15:29:12 +0800

    completed terraform set for metafarm ecs

--- terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/tf-apply.sh
+++ terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/tf-apply.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
+echo """
+    #############################
+    ##  Provisioning ECS Roles ##
+    #############################
+""" 
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/1-Role apply -auto-approve -lock=false
+
+
+echo """
+    ############################
+    ##  Provisioning Clusters ##
+    ############################
+""" 
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/2-Cluster apply -auto-approve -lock=false
+
+
+echo """
+    ####################################
+    ##  Provisioning Task Definitions ##
+    ####################################
+""" 
+
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/3-TaskDefinition apply -auto-approve -lock=false
+
+
+
+echo """
+    ############################
+    ##  Provisioning Service  ##
+    ############################
+""" 
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/4-Service apply -auto-approve -lock=false
--- terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/tf-destroy.sh
+++ terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/tf-destroy.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
 
+echo """
+    ##########################
+    ##  Destroying Service  ##
+    ##########################
+""" 
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/4-Service destroy -auto-approve -lock=false
+
+
+
+echo """
+    ###################################
+    ##  Destroying Task Definitions  ##
+    ###################################
+""" 
+
+
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/3-TaskDefinition destroy -auto-approve -lock=false
+
+
+
+echo """
+    ##########################
+    ##  Destroying Cluster  ##
+    ##########################
+""" 
+
+
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/2-Cluster destroy -auto-approve -lock=false
+
+echo """
+    ############################
+    ##  Destroying ECS Roles  ##
+    ############################
+""" 
+
 terraform -chdir=/mnt/d/Users/RouVin/Documents/xctuality/devops/infra-as-code/terraform/provider/aws/environments/metafarms-higher-env/ecs/fargate/1-Role destroy -auto-approve -lock=false
